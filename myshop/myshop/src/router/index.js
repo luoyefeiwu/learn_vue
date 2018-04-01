c498a848ba6a0c5ba7d767c70e090969988bda76
jerry <568384493@qq.com>
2018-04-02 00:25:10 +0800

    add orderSuccess

--- myshop/myshop/src/router/index.js
+++ myshop/myshop/src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/Cart'
 import Address from '@/views/Address'
+import OrderConfirm from '@/views/OrderConfirm'
+import OrderSuccess from '@/views/OrderSuccess'
 
 Vue.use(Router)
 
@@ -23,6 +25,15 @@ export default new Router({
       path: '/address',
       name: 'Address',
       component: Address
+    },
+    {
+      path: '/orderConfirm',
+      name: 'OrderConfirm',
+      component: OrderConfirm
+    }, {
+      path: '/OrderSuccess',
+      name: 'OrderSuccess',
+      component: OrderSuccess
     }
   ]
 })
